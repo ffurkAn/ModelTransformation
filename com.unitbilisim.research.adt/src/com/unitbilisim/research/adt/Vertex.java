0bb04ca2916d57462ececc986c56a97c4f7030db
A. Furkan Tanriverdi <ffurkan.tanriverdi@gmail.com>
2014-11-13 11:14:51 +0200

    + addEdge method in Graph class edited + findEdge method in Vertex class edited

--- com.unitbilisim.research.adt/src/com/unitbilisim/research/adt/Vertex.java
+++ com.unitbilisim.research.adt/src/com/unitbilisim/research/adt/Vertex.java
@@ -247,10 +247,20 @@ public List getOutgoingEdges() {
    * @return e if its a member of the outgoing edges, null otherwise.
    */
   public Edge<T> findEdge(Edge<T> e) {
-    if (outgoingEdges.contains(e))
-      return e;
-    else
-      return null;
+	  if (outgoingEdges.contains(e)){
+	        
+	    	return e;
+	    }
+	    
+	    else if(incomingEdges.contains(e)){
+	    	
+	    	return e;
+	    }
+	    	
+	    else{
+	    	
+	      return null;
+	      }
   }
 
   /**
